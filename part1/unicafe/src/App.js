811d7829e8fab27f0ecd0d734f4f620226a55a03
pdwarkanath <dwarkanath.prabhu@gmail.com>
2022-03-21 20:28:52 +0530

    ex1.7-8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,12 +4,30 @@ const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
 const Display = ({text, value}) => <p>{text} {value}</p>
 
+const Statistics = ({good, neutral, bad}) => {
+  let all = good + neutral + bad
+  let average = (good - bad)/all
+  let positive = good/all*100
+  return (
+  <>
+  <h2>Statistics</h2>
+  <Display text = "good" value = {good} />
+  <Display text = "neutral" value = {neutral} />
+  <Display text = "bad" value = {bad} />
+  <Display text = "all" value = {all} />
+  <Display text = "average" value = {average} />
+  <Display text = "average" value = {positive + ' %'} />
+  </>
+)
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
+  
   return (
     <div>
       <h1>Unicafe</h1>
@@ -18,10 +36,7 @@ const App = () => {
       <Button text = "neutral" onClick = {() => setNeutral(neutral+1)} />
       <Button text = "bad" onClick = {() => setBad(bad+1)} />
       
-      <h2>Statistics</h2>
-      <Display text = "good" value = {good} />
-      <Display text = "neutral" value = {neutral} />
-      <Display text = "bad" value = {bad} />
+      <Statistics good = {good} neutral={neutral} bad={bad} />
     </div>
 
   )
