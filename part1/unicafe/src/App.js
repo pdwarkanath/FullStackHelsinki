5b408013fe1a7eb027e007129dc57e478c7802b2
pdwarkanath <dwarkanath.prabhu@gmail.com>
2022-03-21 20:37:55 +0530

    ex 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,22 +2,25 @@ import { useState } from 'react'
 
 const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
-const StatisticLine = ({text, value}) => <p>{text} {value}</p>
+const StatisticLine = ({text, value}) => <tr><td>{text}</td><td>{value}</td></tr>
 
 const Statistics = ({good, neutral, bad}) => {
   let all = good + neutral + bad
-  let average = (good - bad)/all
-  let positive = good/all*100
   if (all > 0) {
+    let average = (good - bad)/all
+    let positive = good/all*100
     return (
-    <>
-    <StatisticLine text = "good" value = {good} />
-    <StatisticLine text = "neutral" value = {neutral} />  
-    <StatisticLine text = "bad" value = {bad} />
-    <StatisticLine text = "all" value = {all} />
-    <StatisticLine text = "average" value = {average} />
-    <StatisticLine text = "average" value = {positive + ' %'} />
-    </>
+    
+    <table>
+      <tbody>
+        <StatisticLine text = "good" value = {good} />
+        <StatisticLine text = "neutral" value = {neutral} />  
+        <StatisticLine text = "bad" value = {bad} />
+        <StatisticLine text = "all" value = {all} />
+        <StatisticLine text = "average" value = {average} />
+        <StatisticLine text = "positive" value = {positive + ' %'} />
+      </tbody>
+    </table>
   )
   } else {
     return (
